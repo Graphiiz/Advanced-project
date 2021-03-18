f77b69633cf29c6f0fa1976d6fda265ebc91db9e
Graphiiz <65876337+Graphiiz@users.noreply.github.com>
2021-03-18 11:17:32 -0400

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -63,7 +63,7 @@ def create_trainset(dataset_name):
                           transforms.ToTensor(),transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
 
         trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
-        trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=1,pin_memory=True)
+        trainloader = torch.utils.data.DataLoader(trainset, batch_size=256, shuffle=True, num_workers=1,pin_memory=True)
 
         return trainloader
 
