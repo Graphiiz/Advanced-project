4137b71a5da4985f6a0b9eee2373839037614eca
Graphiiz <65876337+Graphiiz@users.noreply.github.com>
2021-04-09 13:52:54 -0400

    Add files via upload

--- TextRNN/config.py
+++ TextRNN/config.py
@@ -3,14 +3,14 @@
 class Config(object):
     embed_size = 300
     hidden_layers = 2
-    hidden_size = 32
+    hidden_size = 128
     bidirectional = True
     output_size = 4
-    max_epochs = 90
+    max_epochs = 100
     lr = 0.25
     batch_size = 128
     max_sen_len = 50 # Sequence length for RNN
-    dropout_keep = 0.8
+    dropout_keep = 0.5
     momentum = 0.9
     seed = 1
     factor = 0.1
