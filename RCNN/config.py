e0ad43b26010d2628c92841611c8bce4bbf589a3
Graphiiz <65876337+Graphiiz@users.noreply.github.com>
2021-04-11 09:01:34 -0400

    Update config.py

--- RCNN/config.py
+++ RCNN/config.py
@@ -3,14 +3,14 @@
 class Config(object):
     embed_size = 300
     hidden_layers = 2
-    hidden_size = 64
+    hidden_size = 128
     output_size = 4
     max_epochs = 100
     hidden_size_linear = 64
-    lr = 0.5
+    lr = 0.1
     batch_size = 128
     max_sen_len = None # Sequence length for RNN
-    dropout_keep = 0.8
+    dropout_keep = 0.4
     momentum = 0.9
     seed = 1
 
